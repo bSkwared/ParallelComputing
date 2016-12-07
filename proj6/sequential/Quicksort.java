73c2f8342a845d9368a23917e1841004da1ae31c
bSkwared <gh@truebearsan.com>
2016-12-07 12:51:03 -0500

    Started Java quicksort

--- proj6/sequential/Quicksort.java
+++ proj6/sequential/Quicksort.java
@@ -1,39 +1,87 @@
-import java.util.Comparator;
 
-public class Quicksort<B extends Comparable<B>> {
+public class Quicksort {
 
     private int hi;
     private int lo;
 
-    private B[] arr;
+    private int[] arr;
 
-    Comparator<B> comp;
-
-    public Quicksort(B[] array) {
-        this(array, 0, array.length-1, Comparator.<B>naturalOrder());
+    public Quicksort(int[] array) {
+        this(array, 0, array.length-1);
     }
 
-    public Quicksort(B[] array, Comparator<B> comparator) {
-        this(array, 0, array.length-1, comparator);
-    }
-
-    public Quicksort(B[] array, int high, int low) {
-        this(array, high, low, Comparator.<B>naturalOrder());
-
-    }
-
-    public Quicksort(B[] array, int high, int low, Comparator<B> comparator) {
+    public Quicksort(int[] array, int low, int high) {
         hi = high;
         lo = low;
 
         arr = array;
-
-        comp = comparator;
     }
-
 
     public void sort() {
-        
+       sort(lo, hi); 
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 
+
+    private void sort (int a, int b) {
+        if (a >= b) {
+            return;
+        }
+
+        int left = a;
+        int right = b - 1;
+
+        int pivot = arr[b]; // get pivot
+
+        while (left <= right) {
+            while (left <= right && arr[left] < pivot) {
+                ++left;
+            }
+
+            while (left <= right && arr[right] > pivot) {
+                --right;
+            }
+
+            if (left <= right) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                ++left;
+                --right;
+            }
+        }
+
+        int temp = arr[left];
+        arr[left] = arr[b];
+        arr[b] = temp;
+
+
+        sort(a, left-1);
+        sort(left+1, b);
+    }
+
+    public static void main(String[] args) {
+
+        int[] a = {5, 2, 8, 3, 6, 2, 5, 8, 3, 0};
+
+        Quicksort q = new Quicksort(a);
+        
+        for (int i : a) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        q.sort();
+        
+        for (int i : a) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
 }
